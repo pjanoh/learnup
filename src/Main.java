24c17b43d76076ded00fda08ece0ba3cc6eed04f
Filipp Voronov <fivoronov@gmail.com>
2021-10-19 10:33:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int discount = 100;
+
+        int total = amount - discount;
+        System.out.println("Итог: " + total);
+        System.out.println("Скидка: " + discount);
+    }
+}
